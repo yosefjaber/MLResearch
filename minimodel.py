8ee946d4bce1b86c8832033da7ebe139bd8e2e10
Yosef Jaber <yusefj816@gmail.com>
2025-02-07 06:27:07 -0500

    Created a 0.60 Standard Error Model

---
+++ minimodel.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class minimodel(nn.Module):
+    def __init__(self, in_features=4, h1=400, h2=8000, h3=400, out_features=1):
+        super().__init__()
+        self.fc1 = nn.Linear(in_features, h1)  
+        self.fc2 = nn.Linear(h1, h2)  
+        self.fc3 = nn.Linear(h2,h3)
+        self.out = nn.Linear(h3, out_features)  
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))  
+        x = F.relu(self.fc2(x))  
+        x = F.relu(self.fc3(x))
+        x = self.out(x)  
+        
+        return x
