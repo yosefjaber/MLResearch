c1826de6abe83187e88749b73e7adcd15c18838a
Yosef Jaber <yusefj816@gmail.com>
2025-02-18 01:16:33 -0500

    Updated clean_data with functions that can clean each column

--- clean_data.py
+++ clean_data.py
@@ -3,25 +3,54 @@ import numpy as np
 
 # Reads the data converts to UTF-8 and removes all blank rows
 df = pd.read_csv("data/original-data.csv", encoding="latin-1")
-before = len(df)
+print(len(df))
 df.to_csv("data/original-data-utf.csv", encoding="utf-8", index=False)
 original = pd.read_csv("data/original-data-utf.csv")
 clean = original.replace('---', np.nan)
 clean = clean.dropna()
+clean = clean.reset_index(drop=True)
+before = len(clean)
+values_to_drop = []
 
-def clean_row_off_tolorence(df,name_of_row,tolerance):
+def clean_row_off_condition(df, name_of_row, func, values_to_drop):
     df_row = df[name_of_row]
-    values_to_drop = []
     
     for i in range(len(df_row)):
-        if i > 0 and abs(df_row[i-1]-df_row[i]) >= tolerance:
+        # Use positional indexing with .iloc
+        if i > 0 and func(float(df_row.iloc[i])):
+            values_to_drop.append(i)
+            #print(df_row.iloc[i])
+        
+    return values_to_drop
+
+def clean_row_off_tolorence(df,name_of_row,tolerance,values_to_drop):
+    df_row = df[name_of_row]
+    
+    for i in range(len(df_row)):
+        if i > 0 and abs(float(df_row.iloc[i-1])-float(df_row.iloc[i])) >= tolerance:
             values_to_drop.append(i-1)
             values_to_drop.append(i)
-    
-    for i in range(len(values_to_drop)):
-        print(f"df[i-1]: {df[values_to_drop[i]-1]}, df[i]: {df[values_to_drop[i]]}, diff: {abs(df[values_to_drop[i]-1] - df[values_to_drop[i]])}")
-    
-    df = df.drop(values_to_drop)
+            #print(f"{float(df_row.iloc[i])} is i, {float(df_row.iloc[i-1])} is i - 1, diff is {abs(float(df_row.iloc[i-1])-float(df_row.iloc[i]))}")
+    return values_to_drop
 
-print(clean.head())
-clean.to_csv('data/clean-data.csv', index=False)
+#
+
+# values_to_drop = clean_row_off_condition(clean, "Hot Water System : Hot Water Supply Temperature Local (°F)",lambda x : x < 115, values_to_drop)
+
+values_to_drop = clean_row_off_tolorence(clean, "AHU-01 VAV : Hot  Water Coil Flow Meter Local (gpm)", 0.50, values_to_drop)
+values_to_drop = clean_row_off_tolorence(clean, "AHU-01 VAV : Supply Fan Air Flow Local (cfm)", 250, values_to_drop)
+#values_to_drop = clean_row_off_condition(clean, "AHU-01 VAV : Hot  Water Coil Flow Meter Local (gpm)",lambda x : x == 0, values_to_drop)
+values_to_drop = clean_row_off_condition(clean, "Hot Water System : Hot Water Supply Temperature Local (°F)",lambda x : x < 100, values_to_drop)
+
+clean = clean.drop(values_to_drop)
+
+print(clean)
+after = len(clean)
+print(f"Change: {before - after}")
+sum = 0
+for i in range(len(clean["Hot Water System : Hot Water Supply Temperature Local (°F)"])):
+    sum += float(clean["Hot Water System : Hot Water Supply Temperature Local (°F)"].iloc[i])
+print(sum/len(clean["Hot Water System : Hot Water Supply Temperature Local (°F)"]))
+#clean.to_csv('data/clean-data.csv', index=False)
+
+#Delete anything below 100T of in water, also remove delta of 0.5 gpm, remove deltas of 250cfm, remove 2 goofy points
