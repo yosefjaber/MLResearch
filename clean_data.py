c1f7bf9063cc44871c5e0596942d84343d34cc34
Yosef Jaber <yusefj816@gmail.com>
2025-03-03 08:49:09 -0500

    Integrated the second data table into splits to train on

--- clean_data.py
+++ clean_data.py
@@ -2,72 +2,66 @@ import pandas as pd
 import numpy as np
 
 # Reads the data converts to UTF-8 and removes all blank rows
-df = pd.read_csv("data/original-data.csv", encoding="latin-1")
-df.to_csv("data/original-data-utf.csv", encoding="utf-8", index=False)
-original_1 = pd.read_csv("data/original-data-utf.csv")
+original_1 = pd.read_csv("data/original-data-1-utf.csv")
 
-df2 = pd.read_csv("data/original-data-2.csv", encoding="latin-1")
-df2.to_csv("data/original-data-2-utf-head.csv", encoding="utf-8", index=False)
 original_2 = pd.read_csv("data/original-data-2-utf-head.csv")
 original_2 = original_2.drop(["Date", "Time", "Time Zone"], axis=1)
-new_column_order = ['AHU-01 VAV : Discharge Air Temperature (°F)', 
-                   'Hot Water System : Hot Water Supply Temperature Local (°F)',
-                   'AHU-01 VAV : Hot  Water Coil Flow Meter Local (gpm)',
-                   'AHU-01 VAV : Mixed Air Temperature Local (°F)',
-                   'AHU-01 VAV : Total Discharge Airflow (cfm)',]
+new_column_order = ['Discharge Temp(F)', 
+                   'Water Temp(F)',
+                   'Water Flow Rate(gpm)',
+                   'Intake Air Temp(F)',
+                   'Air Flow(cfm)',]
 original_2 = original_2[new_column_order]
 original_2.to_csv("data/original-data-2-utf.csv", encoding="utf-8", index=False)
+original_2 = pd.read_csv("data/original-data-2-utf.csv")
 
-original = pd.concat([original_1,original_2])
-original.to_csv("data/test.csv", encoding="utf-8", index=False)
-
-clean = original.replace('---', np.nan)
-clean = clean.dropna()
-clean = clean.reset_index(drop=True)
-before = len(clean)
-values_to_drop = []
-
-
-# def clean_row_off_condition(df, name_of_row, func, values_to_drop):
-#     df_row = df[name_of_row]
+def clean_row_off_condition(df, name_of_row, func, values_to_drop):
+    df_row = df[name_of_row]
     
-#     for i in range(len(df_row)):
-#         # Use positional indexing with .iloc
-#         if i > 0 and func(float(df_row.iloc[i])):
-#             values_to_drop.append(i)
-#             #print(df_row.iloc[i])
+    for i in range(len(df_row)):
+        # Use positional indexing with .iloc
+        if i > 0 and func(float(df_row.iloc[i])):
+            values_to_drop.append(i)
+            #print(df_row.iloc[i])
         
-#     return values_to_drop
+    return values_to_drop
 
-# def clean_row_off_tolorence(df,name_of_row,tolerance,values_to_drop):
-#     df_row = df[name_of_row]
+def clean_row_off_tolorence(df,name_of_row,tolerance,values_to_drop):
+    df_row = df[name_of_row]
     
-#     for i in range(len(df_row)):
-#         if i > 0 and abs(float(df_row.iloc[i-1])-float(df_row.iloc[i])) >= tolerance:
-#             values_to_drop.append(i-2)
-#             values_to_drop.append(i-1)
-#             values_to_drop.append(i)
-#             values_to_drop.append(i+1)
-#             #print(f"{float(df_row.iloc[i])} is i, {float(df_row.iloc[i-1])} is i - 1, diff is {abs(float(df_row.iloc[i-1])-float(df_row.iloc[i]))}")
-#     return values_to_drop
+    for i in range(len(df_row)):
+        if i > 0 and abs(float(df_row.iloc[i-1])-float(df_row.iloc[i])) >= tolerance:
+            values_to_drop.append(i-2)
+            values_to_drop.append(i-1)
+            values_to_drop.append(i)
+            values_to_drop.append(i+1)
+            #print(f"{float(df_row.iloc[i])} is i, {float(df_row.iloc[i-1])} is i - 1, diff is {abs(float(df_row.iloc[i-1])-float(df_row.iloc[i]))}")
+    return values_to_drop
 
-# #Expects a dataframe and cleans it according to research specifications and returns the clean dataframe
-# def clean_data(df):
-#     df = original.replace('---', np.nan)
-#     df = clean.dropna()
-#     df = clean.reset_index(drop=True)
+#Expects a dataframe and cleans it according to research specifications and returns the clean dataframe
+def clean_data(df):
+    df = df.replace('---', np.nan)
+    df = df.replace('#VALUE!', np.nan)
+    df = df.dropna()
+    df = df.reset_index(drop=True)
     
-#     values_to_drop = []
+    values_to_drop = []
     
-#     values_to_drop = clean_row_off_tolorence(df, "AHU-01 VAV : Hot  Water Coil Flow Meter Local (gpm)", 0.50, values_to_drop)
-#     values_to_drop = clean_row_off_tolorence(df, "AHU-01 VAV : Supply Fan Air Flow Local (cfm)", 250, values_to_drop)
-#     values_to_drop = clean_row_off_condition(df, "Hot Water System : Hot Water Supply Temperature Local (°F)",lambda x : x < 100, values_to_drop)
+    values_to_drop = clean_row_off_tolorence(df, "Water Flow Rate(gpm)", 0.50, values_to_drop)
+    values_to_drop = clean_row_off_tolorence(df, "Air Flow(cfm)", 250, values_to_drop)
+    values_to_drop = clean_row_off_condition(df, "Water Temp(F)",lambda x : x < 100, values_to_drop)
     
-#     values_to_drop = list(set(values_to_drop))
-#     values_to_drop = [i for i in values_to_drop if 0 <= i < len(df)]
-#     df = df.drop(values_to_drop)
+    values_to_drop = list(set(values_to_drop))
+    values_to_drop = [i for i in values_to_drop if 0 <= i < len(df)]
+    df = df.drop(values_to_drop)
     
-#     return df
+    return df
+    
+clean_1 = clean_data(original_1)
+clean_2 = clean_data(original_2)
+clean = pd.concat([clean_1,clean_2])
+clean.to_csv('data/clean-data.csv', index=False)
+
     
 # clean = clean_data(original)
 
