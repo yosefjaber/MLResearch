4302a8b23eeb13651c1fb9a3b1d9a3759b7de196
Yosef Jaber <yusefj816@gmail.com>
2025-03-06 05:49:52 -0500

    Experiment Complete

--- test.py
+++ test.py
@@ -2,17 +2,13 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from evaluate import eval_model
+from evaluate import eval_model_no_side_effect
 from tarikmodel3 import tarikModel3
 from train_model import train_model
 from create_model import create_model
 from experiment import run_experiment
 import gc
 
-bruh = create_model(4,[1024 for _ in range(18)],1)
+bruh = create_model(4, [64, 128, 192, 256, 320, 384, 448, 512, 576, 640, 704, 768, 832, 896, 960, 1024, 1088, 1152, 1216], 1)
 
-# #train_model(test_blockRAM, 0.0001, "AdamW", 450_000, "test_blockRAM.pt") #Use 600_000 lr = 0.000004 2.1
-# train_model(bruh, 1e-5, "SGD", 100, "bruh.pt", momentum=0.7)
-# eval_model(bruh,"bruh.pt", True)
-# torch.cuda.empty_cache()
-
-run_experiment("bruh.pt", bruh, 0.001,"AdamW", 10)
+eval_model_no_side_effect(bruh,"medium_layers_left_pyramid_100_5e-05_AdamW.pt", True)
