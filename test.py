13cdbe19b4f72f7fc56edb0e5103d796dc79c815
Yosef Jaber <yusefj816@gmail.com>
2025-05-07 14:12:51 -0400

    Added SHAP graph

--- test.py
+++ test.py
@@ -11,9 +11,9 @@ import gc
 from experiment_helper import extract_mse_from_file, write_lines, get_count_from_file
 from auto_trainer import auto_train_model
 
-test = create_model(4,[2048 for i in range(18)],1)
+test = create_model(4,[4,8,16,32,50,150,250,350,450,550,650,750,850,950,1150,1250,1350],1)
 
-trained_model = auto_train_model(test, 1e-2, "AdamW", 0.3, "test.pt",  batch_size=5000)
+trained_model = auto_train_model(test, 3e-8, "AdamW", 0.25, "test.pt",  batch_size=500)
 data = eval_model_no_name(trained_model, "test.pt", False)
 original_MSE = extract_mse_from_file("test.pt.txt")
 count = 0
